f14032ea3266f9f34f9fddf7c1272cd6de502288
qodot <waitingforqodot@gmail.com>
2017-05-24 00:27:10 +0900

    Fix fetching session function structure

--- src/components/SignInCallback.vue
+++ src/components/SignInCallback.vue
@@ -8,32 +8,27 @@ import store from '../store'
 
 export default {
   mounted () {
-    this.fetchSession()
+    const token = this.$route.query.token
+    const apiSig = md5(`api_key${process.env.LASTFM_API_KEY}methodauth.getSessiontoken${token}5e3113279a15f96d96cf9e65fd8a985b`)
+
+    fetch(`${process.env.LASTFM_API_URL}?method=auth.getSession&token=${token}&api_key=${process.env.LASTFM_API_KEY}&api_sig=${apiSig}`)
+    .then(resp => resp.text())
+    .then((xml) => {
+      xml2js.parseString(xml, (err, result) => {
+        if (err) {
+          console.log(`session parse error: ${err}`)
+        }
+
+        const session = result.lfm.session[0]
+        store.setUser(session.name[0])
+        store.setSession(session.key[0])
+      })
+    })
+    .catch(err => console.log(err))
 
     this.$router.push({
       name: 'index'
     })
-  },
-  methods: {
-    fetchSession: function () {
-      const token = this.$route.query.token
-      const apiSig = md5(`api_key${process.env.LASTFM_API_KEY}methodauth.getSessiontoken${token}5e3113279a15f96d96cf9e65fd8a985b`)
-
-      fetch(`${process.env.LASTFM_API_URL}?method=auth.getSession&token=${token}&api_key=${process.env.LASTFM_API_KEY}&api_sig=${apiSig}`)
-      .then(resp => resp.text())
-      .then((xml) => {
-        xml2js.parseString(xml, (err, result) => {
-          if (err) {
-            console.log(`session parse error: ${err}`)
-          }
-
-          const session = result.lfm.session[0]
-          store.setUser(session.name[0])
-          store.setSession(session.key[0])
-        })
-      })
-      .catch(err => console.log(err))
-    }
   }
 }
 </script>
