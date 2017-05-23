1446193457ac9cdb317a50f21cde6160e29e3741
qodot <waitingforqodot@gmail.com>
2017-05-23 22:23:53 +0900

    Set global variable for last.fm signin

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,6 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  SIGNIN_REDIRECT: '"http://localhost:8080/signincallback"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,5 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  LASTFM_API_URL: '"http://www.last.fm/api"',
+  LASTFM_API_KEY: '"e2c460467c4b6747ed648d0bfa84463c"'
 }
