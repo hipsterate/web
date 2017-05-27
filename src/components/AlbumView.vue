2716381a0e962d375c0b78bb0c52f48b3ad4775e
qodot <waitingforqodot@gmail.com>
2017-05-27 22:38:57 +0900

    Render album gallery

--- src/components/AlbumView.vue
+++ src/components/AlbumView.vue
@@ -16,11 +16,13 @@ export default {
   created () {
     const params = new Map()
     params.set('user', Cookies.get('username'))
-    params.set('limit', 10)
+    params.set('period', '1month')
 
-    fetch(lastfm.url('user.getRecentTracks', params))
+    fetch(lastfm.url('user.getTopAlbums', params))
     .then(resp => resp.json())
-    .then(json => console.log(json))
+    .then(result => {
+      this.albums = result.topalbums.album.map(album => album.image.slice(-1)[0]['#text'])
+    })
   }
 }
 </script>
