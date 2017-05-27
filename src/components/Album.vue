29d2fdd7898011764148c9d546063f08721f181c
qodot <waitingforqodot@gmail.com>
2017-05-27 23:22:18 +0900

    Remove q-gallery && add flex grid

---
+++ src/components/Album.vue
@@ -0,0 +1,20 @@
+<template>
+  <img class="image" :src="album.image">
+</template>
+
+<script>
+export default {
+  props: ['album'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.image {
+  width: 100%;
+  height: auto;
+}
+</style>
--- src/components/AlbumView.vue
+++ src/components/AlbumView.vue
@@ -1,13 +1,20 @@
 <template>
-  <q-gallery :src="albums">
-  </q-gallery>
+  <div>
+    <div class="album-grid">
+      <album class="album" v-for="album of albums" :key="album.mbid" :album="album"></album>
+    </div>
+  </div>
 </template>
 
 <script>
 import { Cookies } from 'quasar'
 import lastfm from '../lastfm'
+import Album from './Album'
 
 export default {
+  components: {
+    Album
+  },
   data () {
     return {
       albums: []
@@ -21,8 +28,40 @@ export default {
     fetch(lastfm.url('user.getTopAlbums', params))
     .then(resp => resp.json())
     .then(result => {
-      this.albums = result.topalbums.album.map(album => album.image.slice(-1)[0]['#text'])
+      console.log(result.topalbums.album.length)
+      this.albums = result.topalbums.album.map(album => {
+        return {
+          id: album.mbid,
+          name: album.name,
+          image: album.image.slice(-1)[0]['#text'],
+          lastfmLink: album.url,
+          playCount: album.playcount,
+          artistId: album.artist.mbid,
+          artistName: album.artist.name,
+          artistLastfmLink: album.artist.url
+        }
+      })
+      console.log(this.albums.length)
     })
   }
 }
 </script>
+
+<style scoped>
+.album-grid {
+  width: 100%;
+  display: flex;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  flex-flow: row wrap;
+  -ms-flex-flow: row wrap;
+  -webkit-flex-flow: row wrap;
+}
+
+.album-grid>.album {
+  width: 20%;
+  flex: auto;
+  -ms-flex: auto;
+  -webkit-box-flex: 1;
+}
+</style>
