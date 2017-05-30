2a8bf7ca23700ab813de15da00ff71c0acef94c4
qodot <waitingforqodot@gmail.com>
2017-05-30 15:52:09 +0900

    Add album info element
    - appears when mouseenter & disappears when mouseleave

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <img class="image" :src="album.image">
+    <div class="album-container" @mouseenter="info = true" @mouseleave="info = false">
+      <div class="info" v-if="info"></div>
+      <img class="image" :src="album.image">
+    </div>
   </div>
 </template>
 
@@ -9,14 +12,34 @@ export default {
   props: ['album'],
   data () {
     return {
+      info: false
     }
   }
 }
 </script>
 
 <style scoped>
-.image {
+.album-container {
+  position: relative;
   width: 100%;
-  height: auto;
+  height: 100%;
+}
+
+.album-container .info {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+
+  width: 100%;
+  height: 100%;
+  background: #000;
+  opacity: 0.5;
+}
+
+.album-container .image {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 </style>
