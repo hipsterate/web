b68899c257d39a944a60626e581f19f6db502fb4
qodot <waitingforqodot@gmail.com>
2017-06-26 10:41:47 +0900

    Use https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
-  LASTFM_AUTH_PAGE: '"http://www.last.fm/api"',
-  LASTFM_API_URL: '"http://ws.audioscrobbler.com/2.0"',
+  LASTFM_AUTH_PAGE: '"https://www.last.fm/api"',
+  LASTFM_API_URL: '"https://ws.audioscrobbler.com/2.0"',
   LASTFM_API_KEY: '"e2c460467c4b6747ed648d0bfa84463c"',
   LASTFM_API_SECRET: '"5e3113279a15f96d96cf9e65fd8a985b"'
 }
