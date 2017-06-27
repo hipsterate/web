f9b33adda6750b2db9ba984116f245921624a30a
qodot <waitingforqodot@gmail.com>
2017-06-27 16:57:21 +0900

    Fix API proxy object can take args

--- src/utils/api.js
+++ src/utils/api.js
@@ -43,26 +43,36 @@ const firebaseApi = {
     return new Promise((resolve, reject) => {
       firebaseApp.auth().onAuthStateChanged(user => resolve(user))
     })
+  },
+  getFromDB (url) {
+    return new Promise((resolve, reject) => {
+      firebaseApp.database().ref(url)
+      .once('value')
+      .then(result => resolve(result))
+      .catch(error => reject(error))
+    })
   }
 }
 
 const handler = {
   get (target, key, receiver) {
     loadingManager.start()
+    const originProperty = Reflect.get(target, key, receiver)
 
-    const proxiedApi = new Promise((resolve, reject) => {
-      Reflect.get(target, key, receiver)()
-      .then(result => {
-        resolve(result)
-        loadingManager.end()
+    return (...args) => {
+      const proxiedApi = new Promise((resolve, reject) => {
+        originProperty.apply(this, args).then(result => {
+          resolve(result)
+          loadingManager.end()
+        })
+        .catch(error => {
+          reject(error)
+          loadingManager.end()
+        })
       })
-      .catch(error => {
-        reject(error)
-        loadingManager.end()
-      })
-    })
 
-    return () => proxiedApi
+      return proxiedApi
+    }
   }
 }
 
