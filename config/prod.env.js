1254d81b67082ed243c52e46b4feab6c82b8075e
qodot <waitingforqodot@gmail.com>
2017-07-01 14:14:12 +0900

    Add lastfm callback config to production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
+  LASTFM_SIGNIN_CB: '"https://hipsterate.com/lastfmsignincb"',
   LASTFM_AUTH_PAGE: '"https://www.last.fm/api"',
   LASTFM_API_URL: '"https://ws.audioscrobbler.com/2.0"',
   LASTFM_API_KEY: '"e2c460467c4b6747ed648d0bfa84463c"',
