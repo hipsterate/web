f3edeb68cf45875678578d0631756ae49b33f594
qodot <waitingforqodot@gmail.com>
2017-07-01 20:15:07 +0900

    Add watch order prop to change sort order

--- src/components/me/rated/ByTime.vue
+++ src/components/me/rated/ByTime.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <div class="album-grid" v-if="albums.length">
-      <album class="album" v-for="album of sortedAlbums" :key="albumKey(album)" :album="album" :isPlayCount="false"></album>
-    </div>
-    <div v-else>
-      아직 평가한 앨범이 없어요 ㅜㅅㅜ<br>
-      위의 "최근 들은 앨범" 메뉴로 이동해서 평가를 시작하세요!
-    </div>
+    <template v-if="sortedAlbums !== null"> 
+      <div class="album-grid" v-if="albums.length">
+        <album class="album" v-for="album of sortedAlbums" :key="albumKey(album)" :album="album" :isPlayCount="false"></album>
+      </div>
+      <div v-else>
+        아직 평가한 앨범이 없어요 ㅜㅅㅜ<br>
+        위의 "최근 들은 앨범" 메뉴로 이동해서 평가를 시작하세요!
+      </div>
+    </template>
   </div>
 </template>
 
@@ -19,18 +21,31 @@ export default {
   components: {
     Album
   },
+  data () {
+    return {
+      sortedAlbums: null
+    }
+  },
   methods: {
     albumKey (album) {
       return md5(`${album.artistName}${album.name}`)
+    },
+    sortAlbums () {
+      if (this.order === 'asc') {
+        this.sortedAlbums = this.albums.sort((a, b) => a.updatedAt - b.updatedAt)
+      }
+      else {
+        this.sortedAlbums = this.albums.sort((a, b) => b.updatedAt - a.updatedAt)
+      }
+    }
+  },
+  watch: {
+    order () {
+      this.sortAlbums()
     }
   },
   created () {
-    if (this.order === 'asc') {
-      this.sortedAlbums = this.albums.sort((a, b) => a.updatedAt - b.updatedAt)
-    }
-    else {
-      this.sortedAlbums = this.albums.sort((a, b) => b.updatedAt - a.updatedAt)
-    }
+    this.sortAlbums()
   }
 }
 </script>
