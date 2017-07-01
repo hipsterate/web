a848a4f9b96819b3733be6719ffb0943c7b11df1
qodot <waitingforqodot@gmail.com>
2017-07-01 22:02:44 +0900

    Add video background at home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,51 @@
 <template>
-  <div class="home-container">
-    <h4>Hipsterate!</h4>
+  <div>
+    <div class="home-container">
+    </div>
+
+    <iframe id="video-background" :src="optionedVideoUrl" frameborder="0" allowfullscreen></iframe>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      videoUrl: 'https://www.youtube.com/embed/TZD7CNRSq28'
+    }
+  },
+  computed: {
+    optionedVideoUrl () {
+      let options = '?'
+      options += 'autoplay=1'
+      options += '&loop=1'
+      options += '&rel=0'
+      options += '&controls=0'
+      options += '&disablekb=1'
+      return this.videoUrl + options
+    }
+  }
 }
 </script>
 
 <style scoped>
 .home-container {
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
   padding: 1em 2em 2em 2em;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+#video-background {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: -100;
 }
 </style>
